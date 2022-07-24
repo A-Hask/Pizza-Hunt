51c22be63bb81585078f9e352bd75d56ef194f8a
Qu33nArlene <ahaskins93@yahoo.com>
2022-07-24 17:53:25 -0400

    added api routes and controllers

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,63 @@
+const { Pizza } = require("../models");
+
+const pizzaController = {
+  // get all pizzas
+  getAllPizza(req, res) {
+    Pizza.find({})
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  //get one pizza by id
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .then((dbPizzaData) => {
+        //if no pizza is found, send 404
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  //createPizza (POST route)
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  //update pizza by id (PUT method)
+  updatePizza({ params, body }, res) {
+    //{new: true parameter} makes it return the new document. without it, it returns the original
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  //delete pizza
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this " });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+};
+
+module.exports = pizzaController;
